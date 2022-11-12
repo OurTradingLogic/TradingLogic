6c659a58b8db954c44088851f635b811166401ec
OurTradingLogic <ourtradinglogic@gmail.com>
2022-11-12 03:54:11 +0000

    Created new file for colab

---
+++ FindSignal.py
@@ -0,0 +1,12 @@
+import Helper.TradingList as tlist
+import Enum.CommonEnum as cenum
+
+def getBollingerBand():
+    #trade_list = com.gettradinglist()
+    #trade_list = com.getfromjson()
+    #trade_list = tlist.gettradinglist(cenum.ExportFrom.JSON)
+    print('test')
+    #print(trade_list)
+    #excel.WriteFromList(trade_list, cons.DATA_FOLDER+'TradingList.xlsx')
+
+getBollingerBand()
